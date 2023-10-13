bc1fbfd6787cd69b8ef0ffc72374bdd15a3755de
NagalakshmiPrabhakaran <110531220+NagalakshmiPrabhakaran@users.noreply.github.com>
2023-10-13 09:23:06 +0530

    Add files via upload

---
+++ Product Development1/Day19/Backend/src/main/java/com/project/disaster/BackendForDisasterApplication.java
@@ -0,0 +1,14 @@
+package com.project.disaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BackendForDisasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForDisasterApplication.class, args);
+	}
+
+}
